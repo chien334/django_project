b719a840e99c8f96e38aa9072486566ec14e080a
chien334 <lequyetchien3498@gmail.com>
2024-06-02 17:11:28 +0700

    [U] Commit code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# django_project
+# Active environment
+.\project_django\Scripts\activate
+# navigate to folder need work
+cd myproject
+# run project
+python manage.py runserver
--- myproject/myapp/tests.py
+++ myproject/myapp/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Product
 
-# Create your tests here.
+#  create all tests here
+class ProductTests(TestCase):
+
+    def setUp(self):
+        Product.objects.create(name="Product 1", description="Description 1", price=19.99)
+
+    def test_product_list_view(self):
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'product/product_list.html')
+        self.assertContains(response, "Our Products", html=True)
+
+    def test_product_detail_view(self):
+        product = Product.objects.get(name="Product 1")
+        url = reverse('detail', args=[product.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'product/product_detail.html')
+        self.assertContains(response, "Product 1", html=True)
+    
+    def test_product_create_view(self):
+        url = reverse('create')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'product/product_form.html')
+        self.assertContains(response, "Create Product", html=True)
+    
+    def test_product_update_view(self):
+        product = Product.objects.get(name="Product 1")
+        url = reverse('update', args=[product.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'product/product_form.html')
+        self.assertContains(response, "Update Product", html=True)
+    
+    def test_product_delete_view(self):
+        product = Product.objects.get(name="Product 1")
+        url = reverse('delete', args=[product.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'product/product_delete.html')
+        self.assertContains(response, "Are you sure you want to delete", html=True)
