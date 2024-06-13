e6fb01bbe5ec551d985a8da658313012ea96357f
chien334 <lequyetchien3498@gmail.com>
2024-06-13 19:12:23 +0700

    update

--- myproject/myapp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Product(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2) 
